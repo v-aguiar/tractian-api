a6a02d7a9269cb423c4a2b894fb10f198c64a77c
v-aguiar <v.aguiar.dev@gmail.com>
2022-10-06 11:52:12 -0300

    feat: add db collections to prisma schema

---
+++ src/config/db.ts
@@ -0,0 +1,10 @@
+﻿import { PrismaClient } from "@prisma/client";
+
+export let prisma: PrismaClient;
+export function connectDb(): void {
+  prisma = new PrismaClient();
+}
+
+export async function disconnectDb(): Promise<void> {
+  await prisma?.$disconnect();
+}
---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,14 @@
+﻿import { prisma } from "@/config/db";
+
+interface CreateCompanyParams {
+  name: string;
+  cnpj: string;
+}
+
+const userRepository = {
+  create: async (data: CreateCompanyParams) => {
+    await prisma.company.create({
+      data,
+    });
+  },
+};
