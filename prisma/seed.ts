a1017bdbd159f1c0b8ccf604504d3a643f09ca72
v-aguiar <v.aguiar.dev@gmail.com>
2022-10-06 22:48:20 -0300

    feat: add db seed file with initial company, users and units data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,57 @@
-﻿
+﻿import { PrismaClient, Unit, User } from "@prisma/client";
+
+const users: Omit<User, "id">[] = [
+  {
+    phoneNumber: "11999999999",
+    name: "Emerson José Pereira",
+    cpf: "123.456.789-00",
+    role: "Maintenece Manager",
+    companyId: null,
+  },
+  {
+    phoneNumber: "11999999998",
+    name: "Roberta dos Anjos",
+    cpf: "987.654.321-00",
+    role: "Maintenece Manager",
+    companyId: null,
+  },
+];
+
+const company = {
+  name: "Freios Supremos",
+  cnpj: "01.234.567./0001-99",
+};
+
+const units: Omit<Unit, "id">[] = [
+  {
+    name: "Unidade 1",
+    address: "Rio de Janeiro, RJ",
+    companyId: null,
+  },
+  {
+    name: "Unidade 2",
+    address: "Salvador, BA",
+    companyId: null,
+  },
+];
+
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  const createdCompany = await prisma.company.upsert({ create: company, update: {}, where: { cnpj: company.cnpj } });
+  console.log("createdCompany: ", createdCompany);
+
+  users.forEach(async (user) => {
+    user.companyId = createdCompany.id;
+    console.log("user: ", user);
+    await prisma.user.upsert({ create: user, update: {}, where: { cpf: user.cpf } });
+  });
+
+  units.forEach(async (unit) => {
+    unit.companyId = createdCompany.id;
+    console.log("unit: ", unit);
+    await prisma.unit.upsert({ create: unit, update: {}, where: { name: unit.name } });
+  });
+};
+
+seed().then(() => prisma.$disconnect());
